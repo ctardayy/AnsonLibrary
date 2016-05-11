155ab62a37353b47cad3a251c3c88c365ee0bd58
Cesar Tardaguila <cesar@yyresearch.com>
2016-05-11 15:44:09 +0800

    initial commit

---
+++ AnsonLibrary/Person.h
@@ -0,0 +1,19 @@
+//
+//  Person.h
+//  AnsonLibrary
+//
+//  Created by Cesar Tardaguila on 5/11/16.
+//  Copyright © 2016 Cesar Tardaguila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@property (nonatomic, copy, readonly) NSString *identifier;
+@property (nonatomic, copy, readonly) NSString *name;
+
+- (instancetype) initWithIdentifier: (NSString *) identifier
+                               name: (NSString *) name;
+
+@end
